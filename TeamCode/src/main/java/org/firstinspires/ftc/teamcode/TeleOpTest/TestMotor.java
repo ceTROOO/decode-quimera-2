a8a9a2e2fbe5e65d713b19a6c454f5b4c850c8f4
GnHk28 <Gustavo.nhavida@gmail.com>
2026-05-08 18:18:19 -0300

    Teste de motor(APENAS PARA VER SE ANDA) 08/05 sexta

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest/TestMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest/TestMotor.java
@@ -1,4 +1,44 @@
 package org.firstinspires.ftc.teamcode.TeleOpTest;
 
-public class TestMotor {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import java.util.function.Supplier;
+
+@TeleOp (name = "Teste dos Motores", group = "TeleOp")
+
+public class TestMotor extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() {
+        DcMotor frontLeft = hardwareMap.get(DcMotor.class, "fl");
+        DcMotor frontRight = hardwareMap.get(DcMotor.class, "fr");
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double drive = gamepad1.left_stick_x;
+            double strafe = gamepad1.left_stick_y;
+            double giro = gamepad1.right_stick_x;
+
+            double frontLeftPower = drive + strafe + giro;
+            double frontRightPower = drive - strafe - giro;
+
+            frontLeft.setPower(frontLeftPower);
+            frontRight.setPower(frontRightPower);
+
+            telemetry.addData ("Motor", "Esq: %.2f, Dir: %.2f", drive+giro, drive-giro);
+            telemetry.update();
+
+        }
+    }
 }
