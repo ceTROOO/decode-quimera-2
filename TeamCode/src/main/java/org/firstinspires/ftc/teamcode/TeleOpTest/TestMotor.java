c365cc4c2871cade2f93ce1c0f4f604e2647db54
joao <joaossantoss09@gmail.com>
2026-05-18 14:33:27 -0300

    Programação dos outro motores, exclusao do botao A, telemetria dos ticks na tela do driver, motor rodando liso

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest/TestMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest/TestMotor.java
@@ -14,15 +14,18 @@ import java.util.function.Supplier;
 @TeleOp (name = "Teste do Motor", group = "TeleOp")
 public class TestMotor extends LinearOpMode {
 
-    int ticksAlvoDesejado = 1000;
 
     @Override
     public void runOpMode() {
         DcMotor frontLeft = hardwareMap.get(DcMotor.class, "frontLeft"); // COLOCAR ESSE NA PORTA 0 DO CONTROL
         DcMotor frontRight = hardwareMap.get(DcMotor.class, "frontRight"); // COLOCAR ESSE NA PORTA 1 DO CONTROL
+        DcMotor backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        DcMotor backRight = hardwareMap.get(DcMotor.class, "backRight");
 
         frontLeft.setDirection(DcMotor.Direction.FORWARD); // motor 0 no control
-        frontRight.setDirection(DcMotor.Direction.REVERSE); // motor 1 no control
+        frontRight.setDirection(DcMotor.Direction.FORWARD); // motor 1 no control
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
 
         frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -38,37 +41,21 @@ public class TestMotor extends LinearOpMode {
             double strafe = gamepad1.left_stick_y;
             double giro = gamepad1.right_stick_x;
 
-            if (gamepad1.a){
-                 frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                 frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-                 frontLeft.setTargetPosition(ticksAlvoDesejado);
-                 frontRight.setTargetPosition(ticksAlvoDesejado);
-
-
-
-                frontRight.setPower(1);
-                frontLeft.setPower(1);
-
-                while (opModeIsActive() && (frontLeft.isBusy() && frontRight.isBusy())) {
-                    telemetry.addData("status", "Movendo por Encoder...");
-                    telemetry.addData("esq Atual", frontLeft.getCurrentPosition());
-                    telemetry.addData("dir Atual", frontRight.getCurrentPosition());
-                    telemetry.update();
-                }
-            }frontLeft.setPower(0);
-            frontRight.setPower(0);
-
-            frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
             double frontLeftPower = drive + strafe + giro;
             double frontRightPower = drive - strafe - giro;
+            double backLeftPower = drive - strafe + giro;
+            double backRightPower = drive + strafe - giro;
 
             frontLeft.setPower(frontLeftPower);
             frontRight.setPower(frontRightPower);
+            backLeft.setPower(backLeftPower);
+            backRight.setPower(backRightPower);
 
             telemetry.addData ("Motor", "Esq: %.2f, Dir: %.2f", drive+giro, drive-giro);
+            telemetry.addData("status", "Movendo por Encoder...");
+            telemetry.addData("esq Atual", frontLeft.getCurrentPosition());
+            telemetry.addData("dir Atual", frontRight.getCurrentPosition());
+            telemetry.update();
             telemetry.update();
 
         }
